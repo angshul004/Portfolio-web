810ffd822e872570fa28c7952fec467871d1c8b6
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2024-09-21 23:41:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("weather-live").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/posts/angshul4_internship-javascript-webdevelopment-activity-7243230059560378368-Lvxy?utm_source=share&utm_medium=member_desktop", "_blank");
+});
+document.getElementById("weather-git").addEventListener("click", function() {
+    window.open("https://github.com/angshul004/PRODIGY_WD_05", "_blank");
+});
+document.getElementById("watch-live").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/posts/angshul4_im-thrilled-to-share-that-ive-completed-activity-7242216748886753281-Vy_A?utm_source=share&utm_medium=member_desktop", "_blank");
+});
+document.getElementById("watch-git").addEventListener("click", function() {
+    window.open("https://github.com/angshul004/PRODIGY_WD_02", "_blank");
+});
+document.getElementById("landing-live").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/posts/angshul4_im-excited-to-share-that-ive-successfully-activity-7241546439640670208-PhDQ?utm_source=share&utm_medium=member_desktop", "_blank");
+});
+document.getElementById("landing-git").addEventListener("click", function() {
+    window.open("https://github.com/angshul004/PRODIGY_WD_01", "_blank");
+});
+
+document.getElementById("clg-web").addEventListener("click", function() {
+    window.open("https://uem.edu.in/uem-kolkata/", "_blank");
+});
+document.getElementById("scl-web").addEventListener("click", function() {
+    window.open("https://school.banglarshiksha.gov.in/ws/website/index/19114601105", "_blank");
+});
